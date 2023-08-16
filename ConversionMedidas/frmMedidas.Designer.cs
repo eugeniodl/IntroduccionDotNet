aecbc44bc5b85423ca94e7a127d74b9dc5dc4ddd
eugeniodl <Eugenio.diaz@fcys.uni.edu.ni>
2023-08-16 10:10:48 -0600

    Completando formulario de control de medidas

--- ConversionMedidas/frmMedidas.Designer.cs
+++ ConversionMedidas/frmMedidas.Designer.cs
@@ -31,97 +31,104 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.txtMetros = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lstR = new System.Windows.Forms.ListView();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(69, 12);
+            this.pictureBox1.Image = global::ConversionMedidas.Properties.Resources.textil;
+            this.pictureBox1.Location = new System.Drawing.Point(229, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(653, 138);
+            this.pictureBox1.Size = new System.Drawing.Size(418, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(344, 163);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(271, 254);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(327, 38);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "CONTROL DE MEDIDAS";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 217);
+            this.label2.Location = new System.Drawing.Point(54, 308);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.Size = new System.Drawing.Size(199, 20);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "INGRESE VALOR EN METROS";
             // 
-            // textBox1
+            // txtMetros
             // 
-            this.textBox1.Location = new System.Drawing.Point(127, 214);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
-            this.textBox1.TabIndex = 3;
+            this.txtMetros.Location = new System.Drawing.Point(259, 305);
+            this.txtMetros.Name = "txtMetros";
+            this.txtMetros.Size = new System.Drawing.Size(125, 27);
+            this.txtMetros.TabIndex = 3;
             // 
-            // button1
+            // btnCalcular
             // 
-            this.button1.Location = new System.Drawing.Point(322, 213);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCalcular.Location = new System.Drawing.Point(639, 301);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(94, 29);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
-            // listView1
+            // lstR
             // 
-            this.listView1.Location = new System.Drawing.Point(57, 263);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(678, 196);
-            this.listView1.TabIndex = 5;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.lstR.Location = new System.Drawing.Point(55, 354);
+            this.lstR.Name = "lstR";
+            this.lstR.Size = new System.Drawing.Size(678, 196);
+            this.lstR.TabIndex = 5;
+            this.lstR.UseCompatibleStateImageBehavior = false;
+            this.lstR.View = System.Windows.Forms.View.List;
             // 
-            // button2
+            // btnLimpiar
             // 
-            this.button2.Location = new System.Drawing.Point(56, 477);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Location = new System.Drawing.Point(54, 568);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(94, 29);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
-            // button3
+            // btnSalir
             // 
-            this.button3.Location = new System.Drawing.Point(173, 477);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 29);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSalir.Location = new System.Drawing.Point(171, 568);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(94, 29);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // frmMedidas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 526);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(886, 625);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lstR);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtMetros);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmMedidas";
-            this.Text = "frmMedidas";
+            this.Text = "Control de medidas";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -133,10 +140,10 @@
         private PictureBox pictureBox1;
         private Label label1;
         private Label label2;
-        private TextBox textBox1;
-        private Button button1;
-        private ListView listView1;
-        private Button button2;
-        private Button button3;
+        private TextBox txtMetros;
+        private Button btnCalcular;
+        private ListView lstR;
+        private Button btnLimpiar;
+        private Button btnSalir;
     }
 }
--- ConversionMedidas/frmMedidas.cs
+++ ConversionMedidas/frmMedidas.cs
@@ -16,5 +16,42 @@ namespace ConversionMedidas
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // Capturando el valor en metros
+            double metros = double.Parse(txtMetros.Text);
+
+            // Realizando conversiones
+            double centimetros = metros * 100;
+            double pulgadas = centimetros / 2.54;
+            double pies = pulgadas / 12;
+            double yardas = pies / 3;
+
+            // Mostrando los resultados de la conversión
+            lstR.Items.Add("** RESUMEN DE CONVERSIONES **");
+            lstR.Items.Add("MEDIDA EN METROS: " + metros.ToString("0.00"));
+            lstR.Items.Add("----------------------------------------");
+            lstR.Items.Add("MEDIDA EN CENTÍMETROS: " + centimetros.ToString("0.00"));
+            lstR.Items.Add("MEDIDA EN PULGADAS: " + pulgadas.ToString("0.00"));
+            lstR.Items.Add("MEDIDA EN PIES: " + pies.ToString("0.00"));
+            lstR.Items.Add("MEDIDA EN YARDAS: " + yardas.ToString("0.00"));
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtMetros.Clear();
+            lstR.Items.Clear();
+            txtMetros.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("¿Está seguro de salir?", "Medidas",
+                                        MessageBoxButtons.YesNo,
+                                        MessageBoxIcon.Information);
+            if (r == DialogResult.Yes)
+                this.Close();
+        }
     }
 }
